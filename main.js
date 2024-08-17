32afd90105b7a7a8f432a06898992d683323967d
YMBelbase <yagyambelbase@gmail.com>
2024-08-17 13:11:35 +0800

    make dynamic pixel boxes

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const rainbow = document.querySelector('#rainbow');
 const clear = document.querySelector('#clear');
 const glider = document.querySelector('#glider');
 const sizeValue = document.querySelector('#sizeValue');
+const leftBox = document.querySelector('.left.box');
+const s = document.querySelector('#sizeValue');
 
 
 
@@ -11,11 +13,37 @@ function updateGlider(){
     currentRange = glider.value;
     sizeValue.textContent = `${currentRange} x ${currentRange}`;
     console.log(currentRange);
+    
+    updateSquareDiv(currentRange);
+    console.log(`Updating grid with ${currentRange}`);
 }
+
 //range selection
 let defaultRange = glider.value;
 console.log(defaultRange);//////////////////////////////
 let currentRange = defaultRange;
 
 glider.addEventListener("input",updateGlider);
-console.log(currentRange);
+
+
+function updateSquareDiv(iteration){
+    if(iteration <= 0){
+        leftBox.innerHTML = '';
+    return;
+    }
+
+        const itemSize = 500/iteration;
+        leftBox.innerHTML ='';
+        for (let i = 0; i < iteration; i++) {
+            // Inner loop for columns
+            for (let j = 0; j < iteration; j++) {
+                const div = document.createElement('div');
+                div.style.width = `${itemSize}px`;  // Set width
+                div.style.height = `${itemSize}px`; // Set height
+                div.classList.add('ok');
+                leftBox.appendChild(div);
+            }
+    }
+}
+
+
